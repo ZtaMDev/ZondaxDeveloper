10a49beefd63745348cc94b6a861b37add13065c
ZtaDev <85247633+ZtaMDev@users.noreply.github.com>
2025-10-08 23:33:30 -0500

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: true,
-  })],
-  site: 'https://ztamdev.github.io',
-  outDir: './docs',
+  integrations: [tailwind({ applyBaseStyles: true })],
+  site: 'https://ZtaMDev.github.io',
+  base: '/',          // opcional, por claridad
+  outDir: './docs',   // correcto para Pages desde /docs
 });
